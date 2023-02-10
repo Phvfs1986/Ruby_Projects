51aeb36c6e161a3b6bbba79960e03feeca08ccf0
Paulo <phvfs19@gmail.com>
2023-02-10 17:25:59 -0300

    add sub_strings. still needs work

---
+++ sub_strings.rb
@@ -0,0 +1,13 @@
+def substrings(string, dictionary)
+  dictionary.reduce(Hash.new(0)) do |result, word|
+    if string.include?(word)
+      result[word] += 1
+    end
+    result
+  end
+end
+
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
